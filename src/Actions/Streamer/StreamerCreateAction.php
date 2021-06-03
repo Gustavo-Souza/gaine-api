8f9836febca63fbe98f048665de4790fe2b052a0
Gustavo Souza <gustavottsouza@gmail.com>
2021-06-03 15:19:51 -0300

    Changes: code refactor on StreamerCreateAction

--- src/Actions/Streamer/StreamerCreateAction.php
+++ src/Actions/Streamer/StreamerCreateAction.php
@@ -4,67 +4,27 @@ declare(strict_types=1);
 
 namespace App\Actions\Streamer;
 
-use App\Actions\ActionResult;
 use App\Actions\Streamer\Validation\StreamerCreateValidation;
-use App\Data\Exception\ModelAlreadyExistsException;
 use App\Data\Repository\StreamerRepositoryInterface;
-use App\Exception\ValidationException;
-use Exception;
-use PDOException;
-use stdClass;
 
 class StreamerCreateAction
 {
     /** @var StreamerRepositoryInterface */
     private $streamerRepository;
-
-
+    
+    
     public function __construct(StreamerRepositoryInterface $streamerRepository)
     {
         $this->streamerRepository = $streamerRepository;
     }
-
-    public function __invoke(array $requestParams): ActionResult
+    
+    public function __invoke(array $requestParams): void
     {
-        try {
-            $params = StreamerCreateValidation::validate($requestParams);
-            return $this->createStreamer($params);
-        } catch (ValidationException $exception) {
-            return $this->badRequest($exception);
-        } catch (ModelAlreadyExistsException $_) {
-            return $this->streamerAlreadyExists();
-        } catch (PDOException $exception) {
-            return $this->error($exception);
-        } catch (Exception $exception) {
-            return $this->error($exception);
-        }
+        $params = StreamerCreateValidation::validate($requestParams);
 
-        return new ActionResult();
-    }
-
-
-    private function createStreamer(stdClass $params): ActionResult
-    {
         $this->streamerRepository->create(
             strtoupper($params->streamer_code),
             $params->streamer_name
         );
-        return new ActionResult(201);
-    }
-
-    private function streamerAlreadyExists(): ActionResult
-    {
-        return new ActionResult(303);
-    }
-
-    private function badRequest(Exception $exception): ActionResult
-    {
-        return new ActionResult(400);
-    }
-
-    private function error(Exception $exception): ActionResult
-    {
-        // TODO: Log error
-        return new ActionResult(500);
     }
 }
